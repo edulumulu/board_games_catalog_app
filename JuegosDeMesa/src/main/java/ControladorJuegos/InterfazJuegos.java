cc687d0f58367134c9d8152a4d54558c1cc4e577
Eduardo Lucas <eduardolucasmunozdelucas@gmail.com>
2025-01-31 11:05:55 +0100

    Update InterfazJuegos.java

--- JuegosDeMesa/src/main/java/ControladorJuegos/InterfazJuegos.java
+++ JuegosDeMesa/src/main/java/ControladorJuegos/InterfazJuegos.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
- */
+
 package ControladorJuegos;
 
 import Excepciones.ExcepcionMia;
@@ -10,15 +7,10 @@ import Excepciones.ExcepcionMia;
  *Interfaz que estructura los métodos que tendran que desarrollar las clases que implementen de ella.
  * En este caso como la clase que la implementa trata con la interfazDa todos han de lanzar la excepciónMias hacia arriba
  * 
- * @author eduardolucasmunozdelucas
+ * @author edualumulu
  */
 public interface InterfazJuegos {
-    
-//    public abstract boolean listadoAlumnos() throws ExcepcionMia;
-//    
+
     public abstract void ingresarJuego() throws ExcepcionMia;
-//    
-//    public abstract void eliminarAlumnos() throws ExcepcionMia;
-//    
-//    public abstract void menuActualizar() throws ExcepcionMia;
+
 }
