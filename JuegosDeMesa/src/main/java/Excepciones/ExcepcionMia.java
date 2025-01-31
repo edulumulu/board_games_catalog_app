fdbe03132e4e1561f1c4266835b8671528a78460
Eduardo Lucas <eduardolucasmunozdelucas@gmail.com>
2025-01-31 11:06:24 +0100

    Update ExcepcionMia.java

--- JuegosDeMesa/src/main/java/Excepciones/ExcepcionMia.java
+++ JuegosDeMesa/src/main/java/Excepciones/ExcepcionMia.java
@@ -1,14 +1,11 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package Excepciones;
 
 /**
  *Clase que hereda de Excepcion y que configura las excepciones personales que crearé relaccionada con la conexión
  * la modificación y las querys realizadas en la base de datos
  * 
- * @author eduardolucasmunozdelucas
+ * @author edulumulu
  */
 public class ExcepcionMia extends Exception{
     
